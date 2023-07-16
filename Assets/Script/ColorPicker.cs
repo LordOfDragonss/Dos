3f34434f5a74f52717a59dd6061e29ab88c73d06
MikeTool <miketooltje@gmail.com>
2023-07-16 21:52:53 +0200

    fixed the temp color card for all colors

--- Assets/Script/ColorPicker.cs
+++ Assets/Script/ColorPicker.cs
@@ -25,7 +25,7 @@ public class ColorPicker : MonoBehaviour
     {
         Destroy(CardStack.instance.topCard);
         CardStack.instance.topCard = Instantiate(redCard, CardStack.instance.gameObject.transform );
-        CardStack.instance.topCard.transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f / 10, transform.position.z);
+        CardStack.instance.topCard.transform.position = new Vector3(CardStack.instance.gameObject.transform.position.x, CardStack.instance.gameObject.transform.position.y + 0.1f / 10, CardStack.instance.gameObject.transform.position.z);
         //CardStack.instance.topCard.GetComponent<Card>().color = CardSettings.Colors.red;
         GameSettings.instance.activePlayer.isSelectingColor = false;
         GameOptions.instance.EndTurn();
@@ -34,7 +34,7 @@ public class ColorPicker : MonoBehaviour
     {
         Destroy(CardStack.instance.topCard);
         CardStack.instance.topCard = Instantiate(blueCard, CardStack.instance.gameObject.transform);
-        CardStack.instance.topCard.transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f / 10, transform.position.z);
+        CardStack.instance.topCard.transform.position = new Vector3(CardStack.instance.gameObject.transform.position.x, CardStack.instance.gameObject.transform.position.y + 0.1f / 10, CardStack.instance.gameObject.transform.position.z);
         //CardStack.instance.topCard.GetComponent<Card>().color = CardSettings.Colors.blue;
         GameSettings.instance.activePlayer.isSelectingColor = false;
         GameOptions.instance.EndTurn();
@@ -43,7 +43,7 @@ public class ColorPicker : MonoBehaviour
     {
         Destroy(CardStack.instance.topCard);
         CardStack.instance.topCard = Instantiate(greenCard, CardStack.instance.gameObject.transform);
-        CardStack.instance.topCard.transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f / 10, transform.position.z);
+        CardStack.instance.topCard.transform.position = new Vector3(CardStack.instance.gameObject.transform.position.x, CardStack.instance.gameObject.transform.position.y + 0.1f / 10, CardStack.instance.gameObject.transform.position.z);
         //CardStack.instance.topCard.GetComponent<Card>().color = CardSettings.Colors.green;
         GameSettings.instance.activePlayer.isSelectingColor = false;
         GameOptions.instance.EndTurn();
